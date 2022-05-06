2c3280d37c0ae08929965371d1e3a49cf11c155c
GiuliaV <valente.giulia93@gmail.com>
2022-05-06 16:29:04 +0200

    Script apre/chiude hamburger menu + img logo

---
+++ main.js
@@ -0,0 +1,31 @@
+//Esercizio Hamburger menu: mostrare / nascondere il menu principale (mobile)
+
+
+
+// Mostrare il menù principale:
+    
+    // document.querySelector ritorna il primo element nel documento che combacia con il selettore specificato, in questo caso fa-bars.
+    
+    // .addEventListener() aggancia un funzione che sarà chiamata quando accade un evento, in questo caso "click"
+    
+    // la funzione richiama su .hamburger-menu .classList (restituisce i nomi delle classi CSS associate all’elemento)
+   
+document.querySelector(".fa-bars").addEventListener("click", 
+   
+function () {
+   
+    document.querySelector(".hamburger-menu").classList.toggle("active");
+   
+    }
+);
+
+// Nascondere il menù principale:
+
+document.querySelector(".fa-times").addEventListener("click", 
+   
+    function () {
+    
+        document.querySelector(".hamburger-menu").classList.toggle("active");
+   
+    }
+); 
